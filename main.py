6962816ccb25ef508dd3d080416066741ae630c4
Muhammad Kaleem <azaadg295@gmail.com>
2026-05-19 11:23:00 +0500

    Add FastAPI endpoint to read JSON data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import json
 from fastapi import FastAPI
+from fastapi import File, UploadFile
 
 app = FastAPI()
 
@@ -6,12 +8,34 @@ app = FastAPI()
 async def read_root():
     return {"message": "Welcome to FastAPI"}
 
-@app.get('/items/{item_id}')
-async def read_items(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
 # Dynamic Route
-@app.get("/users/{user_id}")
-async def get_user(user_id:int):
-    return {"user_id": user_id}
+@app.get('/items/{item_id}')
+async def read_items(item_id: int):
+    return {"item_id": item_id}
+
+
+# Query Parameters
+@app.get("/users/")
+async def get_user(name: str = None):
+    return {"user_name": name}
+
+# Opetional params
+@app.get('/products')
+async def get_products(limit:int = 10):
+    return {
+        "limit": limit
+    }
+
+# Reading Json file show data on web json form
+@app.get("/listings/")
+async def get_property_listings():
+    with open("props_listing.json", 'r', encoding='utf-8') as file:
+        property_listings = json.load(file)
+    return property_listings
+
+@app.get("/listings/offers/")
+async def get_property_offers():
+    with open("props_listing.json", 'r', encoding='utf-8') as file:
+        property_offers = json.load(file)
+        property_offers = property_offers['offers']
+    return property_offers
