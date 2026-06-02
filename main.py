e73a42667e32a90179ef91bb4db4d24fbd48e921
Muhammad Kaleem <azaadg295@gmail.com>
2026-06-02 05:51:50 +0500

    update code, adding some extra features

--- main.py
+++ main.py
@@ -54,6 +54,12 @@ async def get_listing_agent_info(info: str):
             "ListingAgentInfo": "Not Available"
         }
 
+@app.get('/property/info/{id}')
+async def get_listing_info_by_id(id: int):
+    return {
+        "RentalsInfo of ID": property_info[id]
+    }
+
 # query params using request
 @app.get('/listings/property_info/')
 async def get_main_entity(request: Request):
@@ -78,5 +84,32 @@ async def create_rentals(body:RentalListing):
         "Listings": property_info
     }
 
-# who we send data to the server 
+# Update product Rentals data
+@app.put('/update_listing/{listing_id}')
+def update_listing(listing_data:RentalListing, listing_id:int):
+    for index, props_data in enumerate(property_info, start=1):
+        if props_data.get('id') == listing_id:
+            property_info[index] = listing_data.model_dump()
+            return {
+                "status": "Listing Updated Successfully...",
+                "ListingData": listing_data
+            }    
+        # print(f'{index}: {props_data}')
+
+        return {
+            "ERROR": "Property Listing Not found for this ID"
+        }
+
+@app.delete('delete_listing/{listing_id}')
+async def delete_listing(listing_id: int):
+    for index, props_data in enumerate(property_info):
+        if props_data.get('id') == listing_id:
+            removed_listing = property_info.pop(index)
+            return {
+                "status": "Property Listing Deleted Successfully...",
+                "Deleted Listing": removed_listing
+            }
+    return {"Error": "Property Listing for this ID not Exist"}
+
+# how we send data to the server 
 # Body, headers -> request-headers, query -> query-params
